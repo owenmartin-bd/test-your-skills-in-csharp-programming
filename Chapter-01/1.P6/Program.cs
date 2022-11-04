159b7c54a54daa8215ddcb9d9569375c74e0eac4
Owen Andronicus Martin <59408954+owenmartin-bd@users.noreply.github.com>
2022-11-04 15:02:15 +0600

    1.P6

---
+++ Chapter-01/1.P6/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"5.25/0.0 = {5.25 / 0.0}");
+Console.WriteLine($"1.27/-0 = {1.27 / -0.0}");
+Console.WriteLine($"0/-2.5 = {0 / -2.5}");
+Console.WriteLine($"0.0/0.0 = {0.0 / 0.0}");
+Console.WriteLine($"0/5 = {0 / 5}");
+Console.WriteLine($"-0.0 = {-0.0}");
+Console.WriteLine($"-0.0f = {-0.0f}");
+Console.WriteLine($"double.PositiveInfinity = {double.PositiveInfinity}");
+Console.WriteLine($"-5*PositiveInfinity = {-5 * double.PositiveInfinity}");
+Console.WriteLine($"float.PositiveInfinity = {float.PositiveInfinity}");
